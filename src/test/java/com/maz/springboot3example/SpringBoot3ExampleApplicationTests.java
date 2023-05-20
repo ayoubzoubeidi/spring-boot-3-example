d0e01af4eb074428accd846d5b4d1a1994f0deaa
Ayoub <ayoubzoubeidi@gmail.com>
2023-05-20 12:04:24 +0100

    Added postgres test container in test and removed unnecessary properties.
    To run the application in dev, use the SpringBoot3ExampleApplicationTests.main in order to initialize the postgres db, the connection.
    
    Removed db credentials from application.properties because testcontainers classes implement the ConnectionDetails interface so boot can auto map the credentials.

--- src/test/java/com/maz/springboot3example/SpringBoot3ExampleApplicationTests.java
+++ src/test/java/com/maz/springboot3example/SpringBoot3ExampleApplicationTests.java
@@ -1,13 +1,23 @@
 package com.maz.springboot3example;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
-class SpringBoot3ExampleApplicationTests {
-
-    @Test
-    void contextLoads() {
+@Configuration
+public class SpringBoot3ExampleApplicationTests {
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgreSQLContainer() {
+        return (PostgreSQLContainer) new PostgreSQLContainer("postgres:15.1-alpine")
+                .withReuse(true);
     }
-
+    public static void main(String[] args) {
+        SpringApplication
+                .from(SpringBoot3ExampleApplication::main)
+                .with(SpringBoot3ExampleApplicationTests.class)
+                .run(args);
+    }
 }
